123a3e7115a6d8b6c884bd65136be05120271f98
Tyler Driggers <wtdrigg@gmail.com>
2022-12-22 16:06:36 -0600

    Added the approval mode to the app.

---
+++ vc_approval.py
@@ -0,0 +1,106 @@
+import os
+import glob
+import csv
+from xlsxwriter.workbook import Workbook as WriteWorkbook
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+
+class Approval:
+
+    def __init__(self):
+        self.approval_workbook, self.approval_worksheet = self.setup_vendor_list()
+        self.approval_csv_workbook, self.approval_csv_worksheet = self.load_approval_csv()
+        self.approved_vendor_list = []
+
+    @staticmethod
+    def setup_vendor_list():
+        cwd_path = os.getcwd()
+        file_name = cwd_path + "/BCS_Approved_New_Hires.xlsx"
+        try:
+            ex_workbook = load_workbook(cwd_path + "/BCS_Approved_New_Hires.xlsx")
+        except FileNotFoundError:
+            ex_workbook = Workbook()
+            ex_workbook.save(file_name)
+        ex_worksheet = ex_workbook.worksheets[0]
+        return ex_workbook, ex_worksheet
+
+    @staticmethod
+    def load_approval_csv():
+        downloaded_csv_files = glob.glob(os.path.expanduser("~") + "/Downloads/*.csv")
+        most_recent_csv = max(downloaded_csv_files, key=os.path.getctime)
+        temp_workbook = WriteWorkbook(most_recent_csv[:-4] + ".xlsx")
+        temp_worksheet = temp_workbook.add_worksheet()
+        with open(most_recent_csv, 'r') as f:
+            reader = csv.reader((line.replace('\0', '-') for line in f))
+            for count, row in enumerate(reader):
+                for count2, column in enumerate(row):
+                    temp_worksheet.write(count, count2, column)
+        temp_workbook.close()
+        try:
+            ex_workbook_csv = load_workbook(most_recent_csv[:-4] + ".xlsx")
+        except FileNotFoundError:
+            print('NO CSV FOUND')
+            return
+        ex_worksheet_csv = ex_workbook_csv.worksheets[0]
+        return ex_workbook_csv, ex_worksheet_csv
+
+    def get_approval_list(self):
+        for row in self.approval_csv_worksheet:
+            if row[1].row != 1:
+                vendor_tuple = (row[0].value, row[1].value)
+                self.approved_vendor_list.append(vendor_tuple)
+
+    def confirm_approval(self):
+        previous_approval_names = []
+        for row in self.approval_worksheet:
+            approved_vendor_name = row[1].value
+            previous_approval_names.append(approved_vendor_name)
+        for item in self.approved_vendor_list:
+            if item[1] not in previous_approval_names:
+                target_cell_vc_number = self.find_blank_cell_in_column(self.approval_worksheet['A'])
+                self.add_to_excel(target_cell_vc_number, item[0])
+                target_cell_vendor_name = self.find_blank_cell_in_column(self.approval_worksheet['B'])
+                self.add_to_excel(target_cell_vendor_name, item[1])
+
+    # Iterates through the Excel spreadsheet to find and return the coordinates of the first blank cell in the
+    # column identified by the target_column parameter.
+    def find_blank_cell_in_column(self, target_column):
+        column_coord = target_column[0].coordinate
+        for row in self.approval_worksheet.iter_rows(1, 500):
+            for cell in row:
+                if cell.coordinate[0] == column_coord[0]:
+                    if cell.value is None:
+                        return cell.coordinate
+                    else:
+                        continue
+                else:
+                    continue
+
+    # Adds the provided value parameter to the spreadsheet cell located at the provided cell_coord parameter.
+    def add_to_excel(self, cell_coord, value):
+        cell = self.approval_worksheet[cell_coord]
+        cell.value = value
+
+    # Saves all changes made to the Excel spreadsheet and adjust the length of cells so all information is readable.
+    # If this is a newly created spreadsheet it will be named: "BCS_New_vendors_<today's_date>"
+    def format_and_save_excel(self):
+        column_names = ('A', 'B')
+        cwd_path = os.getcwd()
+        file_name = cwd_path + "/BCS_Approved_New_Hires.xlsx"
+        index_counter = 0
+        max_length = 0
+        for column in self.approval_worksheet.columns:
+            length_list = []
+            for cell in column:
+                length = len(str(cell.value))
+                length_list.append(length)
+                max_length = max(length_list)
+            self.approval_worksheet.column_dimensions[column_names[index_counter]].width = (max_length + 2)
+            index_counter += 1
+        self.approval_workbook.save(file_name)
+
+
+if __name__ == '__main__':
+    x = Approval()
+    x.get_approval_list()
--- vc_extract_2_gui.py
+++ vc_extract_2_gui.py
@@ -6,6 +6,7 @@ the primary classes' constructor.
 
 import tkinter as tk
 from vc_extractor import Extractor
+from vc_approval import Approval
 
 
 class ExtractGUI:
@@ -15,18 +16,57 @@ class ExtractGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry('400x225')
-        self.root.title('VC Extract 2')
+        self.root.title('VC Extract 2 - Extraction Mode')
         self.root.resizable(width=False, height=False)
+        self.main_menu = MenuBar(self)
         self.frame = ExtractFrame(self)
         self.entry_boxes = ExtractEntryBox(self)
         self.labels = ExtractLabels(self)
         self.buttons = ExtractButtons(self)
+        self.listbox = None
         self.root.mainloop()
+
+
+class MenuBar:
+
+    def __init__(self, gui_obj):
+        self.gui_obj = gui_obj
+        self.menu_bar = tk.Menu(self.gui_obj.root)
+        self.file = tk.Menu(self.menu_bar, tearoff=0)
+        self.file.add_command(label="Approval Mode", command=self.click_approval)
+        self.file.add_command(label="Extraction Mode", command=self.click_extraction)
+        self.menu_bar.add_cascade(label='File', menu=self.file)
+        self.menu_bar.add_cascade(label='Error Log')
+        self.gui_obj.root.config(menu=self.menu_bar)
+
+    def click_extraction(self):
+        self.gui_obj.root.title('VC Extract 2 - Extraction Mode')
+        self.gui_obj.frame = ExtractFrame(self.gui_obj)
+        self.gui_obj.entry_boxes = ExtractEntryBox(self.gui_obj)
+        self.gui_obj.labels = ExtractLabels(self.gui_obj)
+        self.gui_obj.buttons = ExtractButtons(self.gui_obj)
+        self.gui_obj.root.update()
+
+    def click_approval(self):
+        self.gui_obj.root.title('VC Extract 2 - Approval Mode')
+        self.gui_obj.frame = ApproveFrame(self.gui_obj)
+        self.gui_obj.listbox = ApproveListBox(self.gui_obj)
+        self.gui_obj.buttons = ApproveButtons(self.gui_obj)
+        self.gui_obj.root.update()
 
 
 class ExtractFrame:
 
-    # Constructor builds the frame object and places it in the GUI root.
+    # Constructor builds the extraction frame object and places it in the GUI root.
+    def __init__(self, gui_obj):
+        self.gui_obj = gui_obj
+        self.main_frame = tk.Frame(self.gui_obj.root, bd=5)
+        self.main_frame.place(anchor='nw', width=400, height=225)
+
+
+class ApproveFrame:
+
+    # Constructor builds the approval frame object and places it in the GUI root.
     def __init__(self, gui_obj):
         self.gui_obj = gui_obj
         self.main_frame = tk.Frame(self.gui_obj.root, bd=5)
@@ -57,6 +97,14 @@ class ExtractLabels:
         self.status_label.place(anchor='nw', x=150, y=110)
         self.load_label = tk.Label(self.gui_obj.frame.main_frame, text='')
         self.load_label.place(anchor='nw', x=125, y=110)
+
+
+class ApproveListBox:
+
+    def __init__(self, gui_obj):
+        self.gui_obj = gui_obj
+        self.approval_list = tk.Listbox(self.gui_obj.frame.main_frame, height=9, width=60)
+        self.approval_list.place(anchor='nw', x=10, y=10)
 
 
 class ExtractButtons:
@@ -113,3 +161,43 @@ class ExtractButtons:
         except Exception:
             self.gui_obj.labels.status_label.config(text="Error Detected")
             self.gui_obj.root.update()
+
+
+class ApproveButtons:
+
+    def __init__(self, gui_obj):
+        self.gui_obj = gui_obj
+        self.approval_obj = None
+        self.previous_approval_names = []
+        self.confirm_update_button = tk.Button(self.gui_obj.frame.main_frame, text="Confirm Vendors Approved",
+                                               command=self.press_confirm, state="disabled")
+        self.confirm_update_button.place(anchor='nw', x=10, y=175)
+        self.load_export_button = tk.Button(self.gui_obj.frame.main_frame, text="Load Export", command=self.press_load)
+        self.load_export_button.place(anchor="nw", x=182, y=175)
+        self.quit_button = tk.Button(self.gui_obj.frame.main_frame, text='Quit', width=12, command=self.press_quit)
+        self.quit_button.place(anchor="nw", x=275, y=175)
+
+    # Closes the webdriver and then the GUI
+    def press_quit(self):
+        self.gui_obj.root.destroy()
+
+    def press_load(self):
+        self.approval_obj = Approval()
+        self.approval_obj.get_approval_list()
+        for row in self.approval_obj.approval_worksheet:
+            approved_vendor_name = row[1].value
+            self.previous_approval_names.append(approved_vendor_name)
+        for count, item in enumerate(self.approval_obj.approved_vendor_list):
+            vc_number, vendor_name = item
+            if vendor_name not in self.previous_approval_names:
+                self.gui_obj.listbox.approval_list.insert(count, vendor_name)
+        self.confirm_update_button.config(state="normal")
+
+    def press_confirm(self):
+        self.approval_obj.confirm_approval()
+        self.approval_obj.format_and_save_excel()
+        self.gui_obj.listbox.approval_list.delete(0, 'end')
+        self.confirm_update_button.config(state="disabled")
+
+
+
